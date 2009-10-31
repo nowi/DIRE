61a1fbee1c98b205c5e2617640553c808a672a96
Philipp Nowakowski <p.nowakowski@gmail.com>
2009-11-01 00:02:06 +0100

    lexicalPathOrder working

--- src/main/scala/core/ordering/LiteralComparator.scala
+++ src/main/scala/core/ordering/LiteralComparator.scala
@@ -41,48 +41,16 @@ class ALCLPComparator extends LiteralComparator {
   // *  (i)  f > g and s ≻ ti for all i with 1 ≤ i ≤ n
   // *  (ii) f = g and for some j we have (s1 , . . . , sj −1 ) = (t1 , . . . , tj −! ),
   // *       sj ≻ tj and s ≻ tk for all k with j < k ≤ n (iii) sj ≽ t for some j with 1 ≤ j ≤ m
-  def compare(s: FOLNode, t: FOLNode): Int = {
+  def compare(s: FOLNode, t: FOLNode): Option[Int] = {
 
 
-    def compareArgs(f: FOLNode, g: FOLNode): Integer = {
-      //  f = g and for some j we have (s1 , . . . , sj-1) = (t1 , . . . , tj-1),
-      // sj ≻ tj
-      // and s ≻ tk for all k with j < k ≤ n
-      var result: Integer = -2
-      for (j <- 0 until g.args.length) {
-        val sj = f.args(j)
-        val tj = g.args(j)
-        // same or sj > tj
-        if (compare(sj, tj) >= 0) {
-          // and s ≻ tk for all k with j < k ≤ n
-          var isRestSmaller: Boolean = false
-          for (k <- j until g.args.length) {
-            val tk = g.args(k)
-            if (compare(f, tk) > 0) isRestSmaller = true
-          }
-          if (isRestSmaller) {
-            result = 1
-          }
-
-        }
-
-      }
-
-      if (result == -2) {
-        // no comparision was possible
-        log.warning("Could not compare f : %s with g : %s", f, g)
-      }
-
-      result
-
-    }
 
     //  this = f (s1 , . . . , sm) ≻ g(t1 , . . . , tn ) = that if and only if that is a proper subterm
     if (s == t) {
-      0
+      Some(0)
     } else if (s.containsSubterm(t)) {
       log.info("%s contains %s as subterm", s, t)
-      1
+      Some(1)
     } else {
       // distinct the possible cases
       (s, t) match {
@@ -90,71 +58,33 @@ class ALCLPComparator extends LiteralComparator {
           // f and g are n-ary
           // f > g and s ≻ ti for all i with 1 ≤ i ≤ n
           log.info("%s is N-Ary %s is N-Ary", f, g)
-          if (comparePrecedence(f, g) == 1 && g.args.forall(compare(f, _) == 1)) {
-            log.info("%s has higher precedence then %s and is greater than all args of %s", f, g, g)
-            1
-          } else if (comparePrecedence(f, g) == 1 && g.args.forall(compare(f, _) == 0)) {
-            log.info("%s has higher precedence then %s and all args are equal", f, g)
-            1
-          } else if (comparePrecedence(g, f) == 1 && f.args.forall(compare(_, g) == 1)) {
-            log.info("%s has higher precedence then %s and is greater than all args of %s", g, f, f)
-            -1
-          } else if (comparePrecedence(g, f) == 1 && f.args.forall(compare(g, _) == 1)) {
-            log.info("%s has higher precedence then %s and is greater than all args of %s", g, f, f)
-            -1
-          } else if (comparePrecedence(g, f) == 1 && f.args.forall(compare(g, _) == 0)) {
-            log.info("%s has higher precedence then %s and all args are equal", g, f)
-            -1
-          } else if (comparePrecedence(f, g) == 1 && g.args.forall(compare(_, f) == 1)) {
-            log.info("%s has higher precedence then %s and is greater than all args of %s", f, g, g)
-            1
-          } else if (comparePrecedence(f, g) == 0 && compareArgs(f, g) == 1) {
-            log.info("%s has same precedence then %s and args compare yielded 1", f, g)
-            1
-          } else if (comparePrecedence(g, f) == 0 && compareArgs(g, f) == 1) {
-            log.info("%s has same precedence then %s and args compare yielded 1", g, f)
-            -1
+
+          // test the rules
+          val isGreater = rule1(f, g) || rule2(f, g) || rule3(f, g)
+          val isSmaller = rule1(g, f) || rule2(g, f) || rule3(g, f)
+
+          if (isGreater) {
+            Some(1)
+          } else if (isSmaller) {
+            Some(-1)
           } else {
             log.warning("Could not compare %s with %s", g, f)
-            0
+            None
           }
 
         }
-        //        case (Nary(f), Unary(g)) => {
-        //          log.info("%s is N-Ary %s is UNAry", f, g)
-        //          // (iii) sj ≽ t for some j with 1 ≤ j ≤ m
-        //          if (f.args.exists(compare(_, g) >= 0)) {
-        //            log.info("%s has some args that are greater or equal then %s", f, g)
-        //            1
-        //          } else {
-        //            log.warning("Could not compare f : %s with g : %s", f, g)
-        //            0
-        //          }
-        //
-        //
-        //        }
-        //        case (Unary(f), Nary(g)) => {
-        //          log.info("%s is N-Ary %s is UNAry", g, f)
-        //          // (iii) sj ≽ t for some j with 1 ≤ j ≤ m
-        //          if (g.args.exists(compare(_, f) >= 0)) {
-        //            log.info("%s has some args that are greater or equal then %s", g, f)
-        //            -1
-        //          } else {
-        //            log.warning("Could not compare f : %s with g : %s", f, g)
-        //            0
-        //          }
-        //        }
+
         case (Nary(f), Unary(g)) => {
           log.info("%s is N-Ary %s is UNAry", f, g)
-          1
+          Some(1)
         }
         case (Unary(f), Nary(g)) => {
           log.info("%s is N-Ary %s is UNAry", g, f)
-          -1
+          Some(-1)
         }
         case (Unary(f), Unary(g)) => {
           log.info("%s is UNAry %s is UNAry", g, f)
-          comparePrecedence(f, g)
+          Some(comparePrecedence(f, g))
         }
       }
 
@@ -163,21 +93,97 @@ class ALCLPComparator extends LiteralComparator {
 
   }
 
-  def comparePrecedence(a: FOLNode, b: FOLNode): Int = {
-    // compare lexical
-    val result = a.asInstanceOf[Term].name.compareTo(b.asInstanceOf[Term].name)
-    log.info("Precedence comparison for %s and %s yielded %d", a, b, result)
 
+  def compareArgs(f: FOLNode, g: FOLNode): Option[Int] = {
+    //  f = g and for some j we have (s1 , . . . , sj-1) = (t1 , . . . , tj-1),
+    // sj ≻ tj
+    // and s ≻ tk for all k with j < k ≤ n
+    var result: Option[Int] = None
+    val lengthD = f.args.length - g.args.length
+    if (lengthD == 0) {
+      for (j <- 0 until g.args.length) {
+        val sj = f.args(j)
+        val tj = g.args(j)
+        // same or sj > tj
+        result = compare(sj, tj) match {
+          case Some(r) => {
+            if (r >= 0) {
+              // and s ≻ tk for all k with j < k ≤ n
+              var isRestSmaller: Boolean = false
+              for (k <- j until g.args.length) {
+                val tk = g.args(k)
+                if (compare(f, tk).getOrElse(0) > 0) isRestSmaller = true
+              }
+              if (isRestSmaller) {
+                Some(1)
+              } else None
 
-    if (result > 0) {
-      log.info("%s has higher precedence than %s", a, b)
-    } else if (result < 0) {
-      log.info("%s has higher precedence than %s", b, a)
+            } else None
+
+          }
+          case None => None
+        }
+
+      }
+
+    } else if (lengthD < 0) {
+      result = Some(-1)
+
     } else {
-      log.info("%s has same precedence as %s", a, b)
+      result = Some(1)
+    }
+
+
+    result match {
+      case None => log.warning("Could not compare f : %s with g : %s", f, g)
+      case _ => result
     }
 
     result
+
+  }
+
+
+  def rule1(f: FOLNode, g: FOLNode): Boolean = {
+    // f > g and s ≻ ti for all i with 1 ≤ i ≤ n
+    val result = (comparePrecedence(f, g) == 1 && g.args.forall(compare(f, _).getOrElse(false) == 1))
+    log.info("f=%s,g=%s , RULE 1 evalutaes to %s", f, g, result)
+    result
+  }
+
+
+  def rule2(f: FOLNode, g: FOLNode): Boolean = {
+    //    (ii) f = g and for some j we have (s1 , . . . , sj−1 ) = (t1 , . . . , tj −! ),
+    // sj tj and s tk for all k with j < k ≤ n
+    val result = (comparePrecedence(f, g) == 0 && compareArgs(f, g).getOrElse(false) == 1)
+    log.info("f=%s,g=%s , RULE 2 evalutaes to %s", f, g, result)
+    result
+
+  }
+
+  def rule3(f: FOLNode, g: FOLNode): Boolean = {
+    // sj ≽ t for some j with 1 ≤ j ≤ m
+    val result = f.args.exists(compare(_, g).getOrElse(false) == 1)
+    log.info("f=%s,g=%s , RULE 3 evalutaes to %s", f, g, result)
+    result
+  }
+
+
+  def comparePrecedence(a: FOLNode, b: FOLNode): Int = {
+    // compare lexical
+    val result = a.asInstanceOf[Term].name.compareTo(b.asInstanceOf[Term].name)
+    //    log.info("Precedence comparison for %s and %s yielded %d", a, b, result)
+
+    if (result > 0) {
+      log.info("%s has higher precedence than %s", a, b)
+      1
+    } else if (result < 0) {
+      log.info("%s has higher precedence than %s", b, a)
+      -1
+    } else {
+      log.info("%s has same precedence as %s", a, b)
+      0
+    }
 
   }
 
--- src/test/scala/core/ordering/LiteralComparatorSpec.scala
+++ src/test/scala/core/ordering/LiteralComparatorSpec.scala
@@ -23,6 +23,7 @@ class LiteralComparatorSpec extends Spec with ShouldMatchers {
     val elizabeth = Constant("Elizabeth")
     val a = Function("Knows", List(john, Variable("x")))
     val b = Function("Knows", List(john, jane))
+    val b1 = Function("Knows", List(john))
     val c = Function("Knows", List(Variable("y"), leonid))
     val c1 = Function("Knows1", List(Variable("y"), leonid))
     val d = Function("Knows", List(Variable("y"), Function("Mother", List(Variable("y")))))
@@ -41,24 +42,38 @@ class LiteralComparatorSpec extends Spec with ShouldMatchers {
     //
     //    }
 
+    it("Literals not containing function or predicate.") {
+      comparator.compare(leonid, elizabeth) should equal(Some(1))
+      comparator.compare(elizabeth, leonid) should equal(Some(-1))
+      comparator.compare(leonid, leonid) should equal(Some(0))
+    }
+
     it("Literals containing a function symbol precede literals that do not contain a function symbol.") {
-      comparator.compare(c, leonid) should equal(1)
-      comparator.compare(leonid, c) should equal(-1)
-      comparator.compare(leonid, leonid) should equal(0)
+      comparator.compare(c, leonid) should equal(Some(1))
+      comparator.compare(leonid, c) should equal(Some(-1))
+      comparator.compare(leonid, leonid) should equal(Some(0))
     }
 
     it("Literals containing a function symbol are ordered according to the precedence of the function symbols.") {
-      comparator.compare(c, c1) should equal(-1)
-      comparator.compare(c1, c) should equal(1)
-      comparator.compare(c1, c1) should equal(0)
+      comparator.compare(c, c1) should equal(Some(-1))
+      comparator.compare(c1, c) should equal(Some(1))
+      comparator.compare(c1, c1) should equal(Some(0))
 
     }
 
 
-    it("Literals not containing a function symbol are ordered according to the precedence of the predicate symbols.") {
-      comparator.compare(c, d) should equal(-1)
-      comparator.compare(d, c) should equal(1)
-      comparator.compare(d, d) should equal(0)
+    it("Literals containing nested functions/predicates") {
+      comparator.compare(c, d) should equal(Some(-1))
+      comparator.compare(d, c) should equal(Some(1))
+      comparator.compare(d, d) should equal(Some(0))
+
+
+    }
+
+    it("Functions with different arities") {
+      comparator.compare(b, b1) should equal(Some(1))
+      comparator.compare(b1, b) should equal(Some(-1))
+      comparator.compare(b1, b1) should equal(Some(0))
 
 
     }
