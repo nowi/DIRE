22d8c9a686ff715cf9d2df4e05d9bc395d83e6cc
Philipp Nowakowski <p.nowakowski@gmail.com>
2009-11-26 20:40:01 +0100

    added new Configuration for ordered resolution

--- src/main/scala/core/config/TheoremProvingConfig1.scala
+++ src/main/scala/core/config/TheoremProvingConfig1.scala
@@ -1,9 +1,11 @@
 package core.config
 
 
+import ordering.ALCLPOComparator
 import reduction.{StillmannSubsumer, StandardFactorizer, SubsumptionDeleter, TautologyDeleter}
-import resolution.BinaryResolver
+import resolution.{OrderedResolver, BinaryResolver}
 import rewriting.{Substitutor, VariableRewriter}
+import selection.StandardClauseLiteralSelection
 
 /**
  * User: nowi
@@ -22,6 +24,23 @@ object TheoremProvingConfig1 {
   lazy val factorizer = new StandardFactorizer(this)
   lazy val resolver = new BinaryResolver(this)
   lazy val subsumptionStrategy = new StillmannSubsumer(this)
+
+
+}
+
+object OrderedTheoremProvingConfig1 {
+  lazy val tautologyDeleter = new TautologyDeleter()
+  lazy val variableRewriter = new VariableRewriter()
+  lazy val subsumptionDeleter = new SubsumptionDeleter(this)
+  lazy val standardizer = new Standardizer(this)
+  lazy val unificator = new Unificator(this)
+  lazy val substitutor = new Substitutor(this)
+  lazy val factorizer = new StandardFactorizer(this)
+  lazy val resolver = new OrderedResolver(this)
+  lazy val subsumptionStrategy = new StillmannSubsumer(this)
+  // ordered resolution needs comparator and selection too
+  lazy val comparator = new ALCLPOComparator()
+  lazy val selector = new StandardClauseLiteralSelection()
 
 
 }
