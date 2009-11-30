621e3fea07ff1d874cddb2e13664132c7aa0e8a5
Philipp Nowakowski <p.nowakowski@gmail.com>
2009-11-30 14:54:13 +0100

    added input filtering to spassparser, added sokrates example to spassparser spec

--- src/main/scala/domain/fol/parsers/SPASSParser.scala
+++ src/main/scala/domain/fol/parsers/SPASSParser.scala
@@ -8,25 +8,42 @@ import scala.util.parsing.combinator.syntactical._
  * Time: 17:59:38
  */
 object SPASSParser extends StandardTokenParsers {
-  lexical.delimiters ++= List("(", ")", ").", "[", "]", ".", ". ", ",", ", ", ";")
+  lexical.delimiters ++= List("(", ")", ").", "[", "]", ".", ". ", ",", ", ", ";","{","}")
   lexical.reserved += ("", "exists", "forall", "and", "or", "not", "implies", "implied", "equiv", "clause", "cnf",
           "dnf", "listofclauses", "true", "false", "axioms", "conjectures", "listofformulae",
           "endoflist", "predicate", "subsort", "sort", "freely", "generatedby", "listofdeclarations",
           "listofsymbols", "sorts", "predicates", "functions", "listofdescriptions", "satisfiable", "unsatisfiable",
-          "unknown", "endproblem", "beginproblem", "formula")
+          "unknown", "endproblem", "beginproblem", "formula","name","author","version","logic","status","description","date")
 
 
-  def problem = "beginproblem" ~ "(" ~> ident <~ ")." ~ description ~ ". " ~ logicalpart ~ rep(settings) ~ "endproblem" ~ "."
+  def problem = "beginproblem" ~ "(" ~ ident ~ ")." ~ description ~ ". " ~ logicalpart ~ rep(settings) ~ "endproblem" ~ "."
 
 
-  def description = "listofdescriptions" ~ ". " ~ "endoflist"
+  def description = "listofdescriptions" ~ ". " ~ name ~ author ~ opt(version)~ opt(logic) ~ status ~ desc ~ opt(date)~   "endoflist"
+
+
+  def name = "name" ~ "(" ~ "{" ~ stringLit ~ "}" ~ ")."
+
+  def author = "author" ~ "(" ~ "{" ~ stringLit ~ "}" ~ ")."
+  def version = "version" ~ "(" ~ "{" ~ stringLit ~ "}" ~ ")."
+  def logic = "logic" ~ "(" ~ "{" ~ stringLit ~ "}" ~ ")."
+  def status = "status" ~ "(" ~ logstate ~ ")."
+  def desc = "description" ~ "(" ~ "{" ~ stringLit ~ "}" ~ ")."
+  def date = "date" ~ "(" ~ "{" ~ stringLit ~ "}" ~ ")."
+
+
+  
+
 
   def logstate = "satisfiable" | "unsatisfiable" | "unknown"
 
 
+
+
+
   def logicalpart = symbollist ~ rep(formulalist) ~ rep(clauselist) ~ rep(prooflist)
 
-  def symbollist = "listofsymbols" ~ ". " ~ functions ~ ". " ~ predicates ~ ". " ~ "endoflist" ~ ". "
+  def symbollist = "listofsymbols" ~ ". " ~ opt(functions~ ". ")  ~ opt(predicates~ ". ")  ~ "endoflist" ~ ". "
 
   def declarationlist = "listofdeclarations." ~ rep(declaration) ~ "endoflist"
 
@@ -55,7 +72,7 @@ object SPASSParser extends StandardTokenParsers {
 
   def origintype = "axioms" | "conjectures"
 
-  def label = ident
+  def label = numericLit
 
 
   def term: Parser[Any] = quantsym ~ "(" ~ termlist ~ "," ~ term ~ ")" | symbol ~ "(" ~ repsep(term, ",") ~ ")" | symbol
@@ -69,19 +86,19 @@ object SPASSParser extends StandardTokenParsers {
           "equiv" | ident
 
 
-  def clauselist = "listofclauses(" ~ origintype ~ "," ~ clausetype ~ ")." ~ rep(clause) ~ "endoflist"
+  def clauselist = "listofclauses" ~ "(" ~ origintype ~ ", " ~ clausetype ~ ")." ~ rep(clause) ~ "endoflist"
 
-  def clause = "clause(" ~ opt(cnfclause | dnfclause) ~ opt("," ~ label) ~ ")."
+  def clause = "clause" ~ "(" ~ opt(cnfclause | dnfclause) ~ opt("," ~ label) ~ ")."
 
   def clausetype = "cnf" | "dnf"
 
-  def cnfclause = "forall(" ~ termlist ~ "," ~ cnfclausebody ~ ")" | cnfclausebody
+  def cnfclause = "forall" ~ "(" ~ termlist ~ "," ~ cnfclausebody ~ ")" | cnfclausebody
 
-  def dnfclause = "exists(" ~ termlist ~ "," ~ dnfclausebody ~ ")" | dnfclausebody
+  def dnfclause = "exists" ~ "(" ~ termlist ~ "," ~ dnfclausebody ~ ")" | dnfclausebody
 
-  def cnfclausebody = "or(" ~ repsep(term, ",") ~ ")"
+  def cnfclausebody = "or" ~ "(" ~ repsep(term, ",") ~ ")"
 
-  def dnfclausebody = "and(" ~ repsep(term, ",") ~ ")"
+  def dnfclausebody = "and" ~ "(" ~ repsep(term, ",") ~ ")"
 
 
   def arity = numericLit
@@ -93,7 +110,7 @@ object SPASSParser extends StandardTokenParsers {
 
   def functions = "functions" ~ "[" ~ funs ~ "]"
 
-  def funs: Parser[List[Any]] = repsep(function, ", ")
+  def funs: Parser[List[Any]] = repsep(function, ",")
 
   def function = "(" ~ funsym ~ "," ~ arity ~ ")"
 
@@ -106,9 +123,21 @@ object SPASSParser extends StandardTokenParsers {
   def sort = sortsym
 
 
+  def convertInput(input : String) : String = {
+    // "_" --> ""
+    // remove "'"
+    // "*" --> "'"
+    // remove line breaks
+
+    input.replace("_","").replace("'","").replace("*","'").replace(", ",",").replace("\n"," ")
+
+  }
+
+
+
   def parse(dsl: String) =
     {
-      val tokens = new lexical.Scanner(dsl)
+      val tokens = new lexical.Scanner(convertInput(dsl))
       phrase(problem)(tokens) match {
         case Success(tree, _) => {
           println(tree)
