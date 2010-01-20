17f9c164150a437df7e76ac29d46a295bcb65d17
Philipp Nowakowski <p.nowakowski@gmail.com>
2010-01-20 18:25:56 +0100

    created WestDomain and CuriosityDomain

---
+++ src/main/scala/core/config/CuriosityDomain.scala
@@ -0,0 +1,35 @@
+package core.config
+
+import domain.fol.ast._
+
+/**
+ * User: nowi
+ * Date: 20.01.2010
+ * Time: 12:58:51
+ */
+
+object CuriosityDomain {
+  val x = Variable("x")
+  val y = Variable("y")
+  val z = Variable("z")
+
+  val tuna = Constant("Tuna")
+  val jack = Constant("Jack")
+  val curiosity = Constant("Curiosity")
+  val loves = (x: FOLNode, y: FOLNode) => Predicate("Loves", x, y)
+  val kills = (x: FOLNode, y: FOLNode) => Predicate("Kills", x, y)
+  val cat = (x: FOLNode) => Predicate("Cat", x)
+  val animal = (x: FOLNode) => Predicate("Animal", x)
+  val f = (x: FOLNode) => Predicate("F", x)
+  val g = (x: FOLNode) => Predicate("G", x)
+
+
+  val A1 = StandardClause(animal(f(x)), loves(g(x), x))
+  val A2 = StandardClause(Negation(loves(x, f(x))), loves(g(x), x))
+  val B = StandardClause(Negation(animal(y)), Negation(kills(x, y)), Negation(loves(z, x)))
+  val C = StandardClause(Negation(animal(x)), loves(jack, x))
+  val D = StandardClause(kills(jack, tuna), kills(curiosity, tuna))
+  val E = StandardClause(cat(tuna))
+  val F = StandardClause(Negation(cat(x)), animal(x))
+  val curiosityKilledTunaGoalClause = StandardClause(Negation(kills(curiosity, tuna)))
+}
---
+++ src/main/scala/core/config/WestDomain.scala
@@ -0,0 +1,76 @@
+package core.config
+
+import domain.fol.ast._
+
+/**
+ * User: nowi
+ * Date: 20.01.2010
+ * Time: 13:00:09
+ */
+
+object WestDomain {
+  val x = Variable("x")
+  val y = Variable("y")
+  val z = Variable("z")
+  val west = Constant("West")
+  val nono = Constant("Nono")
+  val m1 = Constant("M1")
+  val america = Constant("America")
+  val sells = (x: FOLNode, y: FOLNode, z: FOLNode) => Predicate("Sells", x, y, z)
+  val weapon = (x: FOLNode) => Predicate("Weapon", x)
+  val american = (x: FOLNode) => Predicate("American", x)
+  val hostile = (x: FOLNode) => Predicate("Hostile", x)
+  val missile = (x: FOLNode) => Predicate("Missile", x)
+  val owns = (x: FOLNode, y: FOLNode) => Predicate("Owns", x, y)
+  val enemy = (x: FOLNode, y: FOLNode) => Predicate("Enemy", x, y)
+
+
+  val C1 = StandardClause(Negation(Predicate("American", x)), Negation(Predicate("Weapon", y)),
+    Negation(Predicate("Sells", x, y, z)), Negation(Predicate("Hostile", z)),
+    Predicate("Criminal", x))
+
+  val C2 = StandardClause(
+    Negation(Predicate("Missile", x)),
+    Negation(Predicate("Owns", nono, x)),
+    Predicate("Sells", west, x, nono)
+    )
+
+  val C3 = StandardClause(
+    Negation(Predicate("Enemy", x, america)),
+    Predicate("Hostile", x)
+    )
+
+
+  val C4 = StandardClause(
+    Negation(Predicate("Missile", x)),
+    Predicate("Weapon", x)
+    )
+
+  val C5 = StandardClause(
+    Predicate("Owns", nono, m1)
+    )
+  val C6 = StandardClause(
+    Predicate("Missile", m1)
+    )
+  val C7 = StandardClause(
+    Predicate("American", west)
+    )
+
+  val C8 = StandardClause(
+    Predicate("Enemy", nono, america)
+    )
+
+  val CriminalWestGoalClause = StandardClause(
+    Negation(Predicate("Criminal", west))
+    )
+
+  val NotCriminalWestGoalClause = StandardClause(
+    Predicate("Criminal", west)
+    )
+
+  val AmericanWestGoalClause = StandardClause(
+    Negation(Predicate("American", west))
+    )
+
+
+}
