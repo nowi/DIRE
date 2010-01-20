d9eec9885ca58d017662d05f484c76f631b862b3
Philipp Nowakowski <p.nowakowski@gmail.com>
2010-01-20 19:23:06 +0100

    added ordered factoring

--- src/main/scala/core/reduction/Factoring.scala
+++ src/main/scala/core/reduction/Factoring.scala
@@ -2,8 +2,11 @@ package core.reduction
 
 import containers.{ClauseStorage, CNFClauseStore}
 import domain.fol.ast._
+import helpers.Logging
+import ordering.LiteralComparison
 import org.slf4j.LoggerFactory
 import rewriting.Substitution
+import selection.LiteralSelection
 
 
 /**
@@ -25,24 +28,84 @@ trait Factoring {
 
 }
 
-class OrderedFactorizer(env: {val unificator: Unify})
-        extends Factoring {
-  // depends on unifier
+class OrderedFactorizer(env: {val unificator: Unify; val standardizer: Standardizing; val substitutor: Substitution; val selector: LiteralSelection; val literalComparator: LiteralComparison})
+        extends Factoring
+                with Logging {
+  val unificator = env.unificator
+  val standardizer = env.standardizer
+  val substitutor = env.substitutor
+  val selector = env.selector
+  val literalComparator = env.literalComparator
 
-  val log = LoggerFactory getLogger (this getClass)
+  override def factorize(clause: FOLClause): FOLClause = {
+    log.trace("Ordered Factoring by {} on clause {}", this, clause)
+    // as long as there are unfiers , rewirte the clause
+    // Aσ is maximal with respect to C σ ∨ Bσ
+    def rule1(premise: FOLClause, a: FOLNode) = {
+      // get the max
 
-  def factorize(clause: FOLClause): FOLClause = {
-    log.trace("Ordered Factoring on clause {} ", clause)
-    clause
+      if (premise.maxLit(literalComparator) == a) {
+        log.warn("Rule 1 answering TRUE , {} is the maxLit of {} ", a, premise)
+        true
+      } else {
+        log.warn("Rule 1 answering FALSE , {} is NOT the maxLit of {} ", a, premise)
+        false
 
+      }
+
+    }
+
+    //(3) nothing is selected in C σ ∨ Aσ ∨ Bσ
+    def rule2(premise: FOLClause) = {
+      if (selector.selectedLiterals(premise).isEmpty) {
+        log.warn("Rule 2 answering TRUE , there are no selected literals in premise {} ", premise)
+        true
+      } else {
+        log.warn("Rule 2 answering FALSE, there are selected literals in premise {} ", premise)
+        false
+      }
+    }
+
+    // first check literals size , has to be at least 3
+    if (clause.literals.size > 2) {
+      val factorizedClauses = (for (a <- clause.literals.toList;
+                                    b <- clause.literals.toList;
+                                    if (a != b);
+                                    (aS, bS) = standardizer.standardizeApart(a, b);
+                                    mgu = unificator.unify(aS, bS);
+                                    if (mgu != None))
+
+      yield {
+          val clauseSubs = substitutor.substitute(mgu, clause)
+          log.warn("Substitued clauses is {}", clauseSubs)
+          val aSubs = substitutor.substitute(mgu, a)
+          log.warn("Subs a is {}", aSubs)
+
+          val bSubs = substitutor.substitute(mgu, b)
+          log.warn("Subs B is {}", bSubs)
+
+          if (rule1(clauseSubs, aSubs) && rule2(clauseSubs)) {
+            log.warn("ORdered Factoring allowed , creating inference .. ")
+            //C σ ∨ Aσ
+            Some(clauseSubs - bSubs)
+          } else None
+
+        })
+
+      log.info("Factorized clauses are {}", factorizedClauses)
+      clause
+
+    } else {
+      clause
+    }
   }
-
 
   def factorize(clause: FOLClause, mgu: Option[Map[Variable, FOLNode]]) = null
 
   def factorize(clauses: ClauseStorage): ClauseStorage = {
+    assert(clauses.size == 1, "ClauseStorage has more than one clause !")
     log.trace("Ordered Factoring on clauses {} ", clauses)
-    clauses
+    CNFClauseStore(factorize(clauses.head))
   }
 
 }
@@ -85,7 +148,7 @@ class StandardFactorizer(env: {val unificator: Unify; val substitutor: Substitut
   }
 
   override def factorize(clauses: ClauseStorage): ClauseStorage = {
-    CNFClauseStore(clauses.clauses.map(factorize(_)))
+    CNFClauseStore(clauses.map(factorize(_)): _*)
   }
 }
 
