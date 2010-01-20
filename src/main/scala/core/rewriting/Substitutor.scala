9c334b5665c62f74344de1acae55fab223f7ef25
Philipp Nowakowski <p.nowakowski@gmail.com>
2010-01-20 19:25:28 +0100

    added reverse substitution functionality , maybe unfinshed

--- src/main/scala/core/rewriting/Substitutor.scala
+++ src/main/scala/core/rewriting/Substitutor.scala
@@ -26,6 +26,9 @@ trait Substitution {
   def substitute(theta: Option[Map[Variable, FOLNode]], node: FOLNode): FOLNode
 
 
+  def reverseSubstitute(theta: Option[Map[Variable, FOLNode]], node: FOLNode): FOLNode
+
+
   /**
    * Note: Refer to Artificial Intelligence A Modern Approach (2nd Edition):
    * page 273.
@@ -39,6 +42,8 @@ trait Substitution {
    *
    */
   def substitute(theta: Option[Map[Variable, FOLNode]], clause: FOLClause): FOLClause
+
+  def reverseSubstitute(theta: Option[Map[Variable, FOLNode]], clause: FOLClause): FOLClause
 
 
 }
@@ -72,6 +77,23 @@ class Substitutor(env: {val variableRewriter: VariableRewriting}) extends Substi
   }
 
 
+  def reverseSubstitute(theta: Option[Map[Variable, FOLNode]], node: FOLNode) = {
+    // use a variable rewriter
+    theta match {
+      case Some(map) => {
+        // we need to reverse the map
+        val reversedMap = (for ((key, value) <- map) yield Map(value -> key)).reduceLeft(_ ++ _)
+
+        variableRewriter.rewrite2(node, reversedMap)
+      }
+      case None => {
+        //        log.trace("No substition map theata specified , not rewriting term : {}" node)
+        node
+      }
+    }
+  }
+
+
   /**
    * Note: Refer to Artificial Intelligence A Modern Approach (2nd Edition):
    * page 273.
@@ -84,6 +106,7 @@ class Substitutor(env: {val variableRewriter: VariableRewriting}) extends Substi
    *         substitution theta to aSentence.
    *
    */
+
   def substitute(theta: Option[Map[Variable, FOLNode]], clause: FOLClause): FOLClause = {
     // use a variable rewriter
     theta match {
@@ -96,5 +119,19 @@ class Substitutor(env: {val variableRewriter: VariableRewriting}) extends Substi
   }
 
 
+  def reverseSubstitute(theta: Option[Map[Variable, FOLNode]], clause: FOLClause) = {
+    theta match {
+      case Some(map) => {
+        // we need to reverse the map
+        val reversedMap = (for ((key, value) <- map) yield Map(value -> key)).reduceLeft(_ ++ _)
+        variableRewriter.rewriteClause2(clause, reversedMap)
+      }
+      case None => {
+        //        log.trace("No substition map theata specified , not rewriting clause : {}" clause)
+        clause
+      }
+    }
+
+  }
 }
 
