cf0b5e0a94a425cadec9b93d96db09984306b57e
Philipp Nowakowski <p.nowakowski@gmail.com>
2010-05-13 20:30:41 +0200

    fixed severe bug in forward subsumer , need to query for all literals of clause

--- src/main/scala/core/reduction/ForwardSubsumption.scala
+++ src/main/scala/core/reduction/ForwardSubsumption.scala
@@ -23,36 +23,34 @@ object ForwardSubsumer extends ForwardSubsumption with Logging {
   implicit def listofFOLNode2FOLClause(literals: List[FOLNode]): FOLClause = ALCDClause(literals)
 
   private def subsumes(clause: FOLClause, allClauses: ClauseStorage)(implicit subsumptionCheck: Subsumption) : Boolean = {
-    // get all clausees , check if we have a index
-    // we need only to query on one literal from the clause because
-    // a subsumer clause will need to be more general in all literals anyway so we cannot miss
-    // this is a case of imperfect filtering
-    val queryLiteral = clause.literals.head
+
 
     val candidateClauses = allClauses match {
     // retrieve based on the index type .. TODO make this check outside this tight loop
     // more in the configuratoin or initialization of the robinson proover !
       case indexedClauseStore: ForwardMatchingGeneralClauseRetrieval => {
-        indexedClauseStore.retrieveForwardMatchingGeneralizations(queryLiteral)
+        // query on all literals of the clause
+        clause.literals.flatMap(indexedClauseStore.retrieveForwardMatchingGeneralizations(_))
       }
 
       case indexedClauseStore: GeneralClauseRetrieval => {
         // next best index is a matching generalziation supporting index
-        indexedClauseStore.retrieveGeneralizations(queryLiteral)
+        clause.literals.flatMap(indexedClauseStore.retrieveGeneralizations(_))
       }
 
       case _ => {
         // no index support
-        log.ifDebug("Performing forward matching without index support !")
         // copy over all clauses from the claus store -- expensive
         allClauses.toList
       }
     }
 
+
+    
     // from those candidates try to find one canddate that subsumes the clause
     candidateClauses.find(subsumptionCheck(_, clause)) match {
       case Some(subsumerClause) => {
-//        log.debug("%s detected that clause %s is subsumed by %s", this, clause,subsumerClause)
+        log.ifDebug("%s detected that clause %s is subsumed by %s", this, clause,subsumerClause)
         true
       }
 
